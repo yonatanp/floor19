43c9e6250878a491dafb1654bbb578ab9c14e791
Shani Kotler <t-shkotl@microsoft.com>
2016-09-23 00:58:23 +0300

    t

--- floor19/TextProcessing.py
+++ floor19/TextProcessing.py
@@ -3,45 +3,49 @@ import numpy as np
 import nltk
 from nltk.stem.lancaster import LancasterStemmer
 from HebrewTokenizer import *
-
-ENGLISH_STOPWORDS = ["at", "a", "about", "above", "above", "across", "after", "afterwards", "again", "against", "all", "almost", "alone", "along", "already", "also","although","always","am","among", "amongst", "amoungst", "amount",  "an", "and", "another", "any","anyhow","anyone","anything","anyway", "anywhere", "are", "around", "as",  "at", "back","be","became", "because","become","becomes", "becoming", "been", "before", "beforehand", "behind", "being", "below", "beside", "besides", "between", "beyond", "bill", "both", "bottom","but", "by", "call", "can", "cannot", "cant", "co", "con", "could", "couldnt", "cry", "de", "describe", "detail", "do", "done", "down", "due", "during", "each", "eg", "eight", "either", "eleven","else", "elsewhere", "empty", "enough", "etc", "even", "ever", "every", "everyone", "everything", "everywhere", "except", "few", "fifteen", "fify", "fill", "find", "fire", "first", "five", "for", "former", "formerly", "forty", "found", "four", "from", "front", "full", "further", "get", "give", "go", "had", "has", "hasnt", "have", "he", "hence", "her", "here", "hereafter", "hereby", "herein", "hereupon", "hers", "herself", "him", "himself", "his", "how", "however", "hundred", "ie", "if", "in", "inc", "indeed", "interest", "into", "is", "it", "its", "itself", "keep", "last", "latter", "latterly", "least", "less", "ltd", "made", "many", "may", "me", "meanwhile", "might", "mill", "mine", "more", "moreover", "most", "mostly", "move", "much", "must", "my", "myself", "name", "namely", "neither", "never", "nevertheless", "next", "nine", "no", "nobody", "none", "noone", "nor", "not", "nothing", "now", "nowhere", "of", "off", "often", "on", "once", "one", "only", "onto", "or", "other", "others", "otherwise", "our", "ours", "ourselves", "out", "over", "own","part", "per", "perhaps", "please", "put", "rather", "re", "same", "see", "seem", "seemed", "seeming", "seems", "serious", "several", "she", "should", "show", "side", "since", "sincere", "six", "sixty", "so", "some", "somehow", "someone", "something", "sometime", "sometimes", "somewhere", "still", "such", "system", "take", "ten", "than", "that", "the", "their", "them", "themselves", "then", "thence", "there", "thereafter", "thereby", "therefore", "therein", "thereupon", "these", "they", "thickv", "thin", "third", "this", "those", "though", "three", "through", "throughout", "thru", "thus", "to", "together", "too", "top", "toward", "towards", "twelve", "twenty", "two", "un", "under", "until", "up", "upon", "us", "very", "via", "was", "we", "well", "were", "what", "whatever", "when", "whence", "whenever", "where", "whereafter", "whereas", "whereby", "wherein", "whereupon", "wherever", "whether", "which", "while", "whither", "who", "whoever", "whole", "whom", "whose", "why", "will", "with", "within", "without", "would", "yet", "you", "your", "yours", "yourself", "yourselves", "the"]
+import json
+ENGLISH_STOPWORDS = []
 HEBREW_STOPWORDS = []
+TFIDF_FILENAME = '../data/ynet_all_types_500_articles_tf_idf.txt'
 
 class TextProcessing(object):
     def __init__(self, text):
         self.text = text
         self.st = LancasterStemmer()
+        for line in open(TFIDF_FILENAME, 'r'):
+            self.idf_dict = json.loads(line)
 
     def getFreqDict(self, hebrew=True):
         if hebrew:
-            cleaned_words_list = self.cleanTextHebrew()
+            cleaned_words_list = self.cleanTextHebrew(self.text)
         else:
-            cleaned_words_list = self.cleanTextEnglish()
+            cleaned_words_list = self.cleanTextEnglish(self.text)
         print "cleaned_words_list", cleaned_words_list
         sum_words = len(cleaned_words_list)
         freq_dict = dict((word, count/float(sum_words)) for word, count in Counter(cleaned_words_list).iteritems())
-        return freq_dict
+        tfidf = {}
+        for d in freq_dict.items():
+            print d[0], "in idf:", self.idf_dict.get(d[0].decode('UTF-8'),0)
+            tfidf[d[0]] = self.idf_dict.get(d[0],0)*d[1]
+        return tfidf
 
-    def cleanTextEnglish(self):
+    def cleanTextEnglish(self, text):
         all_kw = []
-        for w in nltk.word_tokenize(self.text):
+        for w in nltk.word_tokenize(text):
             ww = self.st.stem(w).lower()
             if ww.isalpha and ww not in ENGLISH_STOPWORDS:
                 all_kw.append(ww)
         #### TODO - change all_kw to be a a list of all KW in space
         return self.removeWordNotInSpace(all_kw, all_kw)
 
-    def cleanTextHebrew(self, exclude_punct=False):
+    def cleanTextHebrew(self, text):
         tok_s = []
-        for i in tokenize(self.text):
-            if exclude_punct is True:
-                if i[0] == "HEB":
-                    tok_s.append(i[1])
-            else:
+        for i in tokenize(text):
+            if i[0] == "HEB":
                 tok_s.append(i[1])
-
         return tok_s
 
+   
     def removeWordNotInSpace(self, word_sen, gen_space):
         return np.intersect1d(word_sen, gen_space)
 
